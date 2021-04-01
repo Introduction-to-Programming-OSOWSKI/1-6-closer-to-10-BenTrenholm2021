3a9b24c1aa4490726b9434399066d779aafa7f81
Trenholm Benjamin <814842@stma.k12.mn.us>
2021-04-01 10:14:17 -0500

    close10test1

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-#WRITE YOUR CODE IN THIS FILE
+def close10(x, y):
+    if abs(10 - x) < abs(10 - y):
+        return (x)
+    elif abs(10 - x) > abs(10 - y):
+        return (y)
+print(close10(1000, 80))
